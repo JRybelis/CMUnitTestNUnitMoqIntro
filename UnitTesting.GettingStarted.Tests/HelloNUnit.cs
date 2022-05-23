a063aa244bc3907a1a568301e9e11f27f39680b8
jokubasr <jokubas.rybelis@softra.lt>
2022-05-23 12:51:37 +0300

    First test case.

---
+++ UnitTesting.GettingStarted.Tests/HelloNUnit.cs
@@ -0,0 +1,19 @@
+﻿//  --------------------------------------------------------------------------------------
+// UnitTesting.GettingStarted.Tests.HelloNunit.cs
+// 2022/05/23
+//  --------------------------------------------------------------------------------------
+
+using NUnit.Framework;
+
+namespace UnitTesting.GettingStarted.Tests
+{
+    [TestFixture]
+    public class HelloNUnit
+    {
+        [Test]
+        public void TestHelloNUnit()
+        {
+            Assert.That(true, Is.False);
+        }
+    }
+}
