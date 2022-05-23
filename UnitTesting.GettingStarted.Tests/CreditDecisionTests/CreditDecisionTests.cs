d2f9e6563c43d60169cef598acd35bafb31ecd32
jokubasr <jokubas.rybelis@softra.lt>
2022-05-23 16:13:16 +0300

    Introduce TestCaseData

--- UnitTesting.GettingStarted.Tests/CreditDecisionTests/CreditDecisionTests.cs
+++ UnitTesting.GettingStarted.Tests/CreditDecisionTests/CreditDecisionTests.cs
@@ -3,6 +3,7 @@
 // 2022/05/23
 //  --------------------------------------------------------------------------------------
 
+using System.Collections.Generic;
 using Moq;
 using NUnit.Framework;
 using UnitTesting.GettingStarted.Tests.CreditDecision;
@@ -12,17 +13,29 @@ namespace UnitTesting.GettingStarted.Tests.CreditDecisionTests
     [TestFixture]
     public class CreditDecisionTests
     {
+        public static IEnumerable<TestCaseData> CreditDecisionTestData
+        {
+            get
+            {
+                yield return new TestCaseData(100, "Declined");
+                yield return new TestCaseData(549, "Declined");
+                yield return new TestCaseData(550, "Maybe");
+                yield return new TestCaseData(674, "Maybe");
+                yield return new TestCaseData(675, "We look forward to doing business with you!");
+            }
+        }
         Mock<ICreditDecisionService> mockCreditDecisionService;
         private CreditDecision.CreditDecision systemUnderTest;
 
-        [TestCase(100, "Declined")]
-        [TestCase(549, "Declined")]
-        [TestCase(550, "Maybe")]
-        [TestCase(674, "Maybe")]
-        [TestCase(675, "We look forward to doing business with you!")]
-        public void MakeCreditDecision_Always_ReturnsExpectedResult(int creditScore, string expectedResult)
+        [SetUp]
+        public void SetUp()
         {
             mockCreditDecisionService = new Mock<ICreditDecisionService>(MockBehavior.Strict);
+        }
+
+        [TestCaseSource(typeof(CreditDecisionTests), nameof(CreditDecisionTestData))]
+        public void MakeCreditDecision_Always_ReturnsExpectedResult(int creditScore, string expectedResult)
+        {
             mockCreditDecisionService.Setup(p => p.GetDecision(creditScore)).Returns(expectedResult);
 
             systemUnderTest = new CreditDecision.CreditDecision(mockCreditDecisionService.Object);
