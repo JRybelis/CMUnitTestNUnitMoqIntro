d99ee347b5804f2d0a762daa523a579f4bc491bb
jokubasr <jokubas.rybelis@softra.lt>
2022-05-23 15:08:43 +0300

    Add calculator tests and implementation.

---
+++ UnitTesting.GettingStarted/Calculator.cs
@@ -0,0 +1,15 @@
+﻿//  --------------------------------------------------------------------------------------
+// UnitTesting.GettingStarted.Calculator.cs
+// 2022/05/23
+//  --------------------------------------------------------------------------------------
+
+namespace UnitTesting.GettingStarted
+{
+    public class Calculator
+    {
+        public int Add(int lhs, int rhs)
+        {
+            return lhs + rhs;
+        }
+    }
+}
